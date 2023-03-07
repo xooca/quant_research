bd75e558d967faf3dcbbc347a880aa593979dbcf
prabhat kumar <prabhat8k@gmail.com>
2023-03-07 14:01:21 +0530

    changed dataengine

--- data/custom_signals/performance/rollpercent_change.py
+++ data/custom_signals/performance/rollpercent_change.py
@@ -22,6 +22,8 @@ def rollpercent_change(close, length=None, offset=None, **kwargs):
     if close is None: return
     _name = "ROLLPER"
     _props = f"_{length}_{offset}"
+    
+
     retvalue = close.pct_change(periods=length, fill_method=kwargs['fill_method'], limit=kwargs['limit'], freq=kwargs['freq'])*kwargs['multiplier']
     
     if offset != 0:
