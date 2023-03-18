58fde359cec97e6b09eb3e217cb42a59e0319d87
prabhat kumar <prabhat8k@gmail.com>
2023-03-18 19:39:28 +0530

    added

--- data/custom_signals/statistics/diff_cols.py
+++ data/custom_signals/statistics/diff_cols.py
@@ -24,7 +24,7 @@ def diff_cols(first,second, offset_first=None, offset_second=None,**kwargs):
     offset_second = get_offset(offset_second)
     
     _name = "ROLLPRH"
-    _props = f"_{length}_{offset}_{freq}_{shift_val}_{rsmpl}_{r1.replace(':','')}_{r2.replace(':','')}".replace("-", '_minus_')
+    _props = f"_{length}_{offset_first}_{offset_first}_{freq}_{shift_val}_{rsmpl}_{r1.replace(':','')}_{r2.replace(':','')}".replace("-", '_minus_')
     
     if offset_first != 0:
         first = first.shift(offset_first)
